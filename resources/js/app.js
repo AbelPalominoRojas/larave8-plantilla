11e8f7d38a54c03824dac6a774183f5a6078865d
Abel Palomino <abelpr87@gmail.com>
2022-05-29 10:35:28 -0500

    Configuracion - Js Bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
 require('./bootstrap');
+
+import '@popperjs/core';
+import 'bootstrap';
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,10 +12,53 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
     <div class="container">
         <div class="row">
             <div class="col">
-                <button type="button" class="btn btn-primary"> <i class="fa-solid fa-house-chimney"></i> Primary</button>
+                <button type="button" class="btn btn-primary"> <i class="fa-solid fa-house-chimney"></i>
+                    Primary</button>
                 <button type="button" class="btn btn-secondary">Secondary</button>
                 <button type="button" class="btn btn-success">Success</button>
                 <button type="button" class="btn btn-danger">Danger</button>
@@ -33,31 +76,36 @@
             <div class="col">
                 <div class="alert alert-primary" role="alert">
                     A simple primary alert—check it out!
-                  </div>
-                  <div class="alert alert-secondary" role="alert">
+                </div>
+                <div class="alert alert-secondary" role="alert">
                     A simple secondary alert—check it out!
-                  </div>
-                  <div class="alert alert-success" role="alert">
+                </div>
+                <div class="alert alert-success" role="alert">
                     A simple success alert—check it out!
-                  </div>
-                  <div class="alert alert-danger" role="alert">
+                </div>
+                <div class="alert alert-danger" role="alert">
                     A simple danger alert—check it out!
-                  </div>
-                  <div class="alert alert-warning" role="alert">
+                </div>
+                <div class="alert alert-warning" role="alert">
                     A simple warning alert—check it out!
-                  </div>
-                  <div class="alert alert-info" role="alert">
+                </div>
+                <div class="alert alert-info" role="alert">
                     A simple info alert—check it out!
-                  </div>
-                  <div class="alert alert-light" role="alert">
+                </div>
+                <div class="alert alert-light" role="alert">
                     A simple light alert—check it out!
-                  </div>
-                  <div class="alert alert-dark" role="alert">
+                </div>
+                <div class="alert alert-dark" role="alert">
                     A simple dark alert—check it out!
-                  </div>
-                  <div class="alert alert-teal" role="alert">
+                </div>
+                <div class="alert alert-teal" role="alert">
                     A simple dark alert—check it out!
-                  </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+
             </div>
         </div>
     </div>
